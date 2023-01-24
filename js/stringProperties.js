1bfe8daa113a03abd5292b4440fab0cfeef650f7
Agajan Sahatov <agajan.st@gmail.com>
2023-01-25 01:14:28 +0800

    String Properties

---
+++ js/stringProperties.js
@@ -0,0 +1,15 @@
+const movie = {
+    title: 'a',
+    releaseYear: 2018,
+    rating: 4.5,
+    director: 'b'
+}
+
+showProperties(movie);
+
+function showProperties(object){
+    for (const key in object) {
+        if(typeof object[key] === "string")
+            console.log(key, object[key]);
+    }
+}
